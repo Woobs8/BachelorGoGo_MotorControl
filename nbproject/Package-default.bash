cd74355f23a8f0bf8d6a28faa5821e3ac167ea4a
Rasmus Ørndrup <rasmus_oerndrup@hotmail.com>
2016-11-09 12:50:30 +0100

    Commit That Makes Master Include the Correct files

--- nbproject/Package-default.bash
+++ nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/Bachelor_Motor_Control.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=Bachelor_Motor_Control.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=bachelormotorcontrol.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/BachelorGoGo_MotorControl.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=BachelorGoGo_MotorControl.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=bachelorgogomotorcontrol/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/bachelormotorcontrol.x/bin
+makeDirectory ${TMPDIR}/bachelorgogomotorcontrol/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/bachelormotorcontrol.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/bachelorgogomotorcontrol.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/bachelormotorcontrol.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/bachelorgogomotorcontrol.tar *
 checkReturnCode
 
 # Cleanup
